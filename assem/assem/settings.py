43f855b345ecd98d62a253e938c34eca003571a0
Bekarys Ibrashev <106703788+bekarysibrashev@users.noreply.github.com>
2023-11-26 05:11:29 +0500

    all

--- assem/assem/settings.py
+++ assem/assem/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import environ
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,13 +126,18 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+env = environ.Env()
+
+# Загружаем переменные окружения из файла .env
+environ.Env.read_env()
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587  # Используйте 587 для TLS
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False  # Установите в False
 EMAIL_HOST_USER = 'beka.ibrashev@gmail.com'
-EMAIL_HOST_PASSWORD = 'nnswqafxdescuici'
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 
 SERVER_EMAI = EMAIL_HOST_USER
